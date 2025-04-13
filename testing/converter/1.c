b4081c6bafc95c93d34e5175aa0844201e021cc8
dubya <dubya297@yahoo.com>
2025-04-13 18:47:03 -0500

    Fixed bug where loops would only execute once in the IR.

--- testing/converter/1.c
+++ testing/converter/1.c
@@ -1,22 +1,8 @@
 
-int a = 2;
-
-int add(int a, int b);
-
-int mul(int a, int b){
-    return a * b;
-}
-
-int add(int a, int b){
-    return a + b;
-}
-
-
-int main(int argc, char** argv){
-    int d = add(2, 4);
-    int e = mul(add(2, 3), d);
-
-    char* test = "Hello, world\n";
-
-    return add(e, 1);
+int pow(int x, int n){
+    int result = 1;
+    for (int i=0; i<n; i++){
+        result *= x;
+    }
+    return result;
 }
